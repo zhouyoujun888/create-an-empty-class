67f0f9d8d3f9061d30a0cbe35a3d4516ae345f6d
qiaomengnan16 <285921957@qq.com>
2019-09-28 09:38:18 +0800

    cat (#119)
    cat (#119)

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+/**
+ * @author qiaomengnan
+ * @ClassName: Cat
+ * @Description:
+ * @date 2019-09-28
+ */
+public class Cat {
+
+	public static void main(String[] args) {
+		System.out.println("我是一个Cat");
+	}
+
+}
